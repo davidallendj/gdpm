65be8090d3a969dda446239cf2e014c0191e5860
David J. Allen <davidallendj@gmail.com>
2023-01-04 20:55:11 -0600

    Add `basic.cpp` test file

---
+++ tests/basic.cpp
@@ -0,0 +1,29 @@
+#define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
+
+#include "log.hpp"
+#include "cache.hpp"
+#include "config.hpp"
+
+#include <doctest.h>
+
+
+
+TEST_CASE("Confirm doctest unit testing"){
+	CHECK(true);
+	gdpm::log::print("Testing doctest");
+}
+
+
+TEST_CASE("Test cache database functions"){
+	gdpm::cache::create_package_database()
+}
+
+
+TEST_CASE("Test configuration functions"){
+	gdpm::config::context config;
+	config.path = gdpm::constants::TestPath + "/";
+
+	std::string json = gdpm::config::to_json(config);
+	int status = gdpm::config::save(config);
+	gdpm::config::context new_config = gdpm::config::load(config.path, config)
+}
